cbd618c0a8ab92571ff84f423488a053976dad39
Lorenzo <lorenzocoslop2709@gmail.com>
2024-10-25 10:52:43 -0300

    Testando

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
-<?php
-header("https://671b9eba1328ed24300633fe--benevolent-caramel-20ac44.netlify.app/Frontex/public_html/view/v-home.php");
+
+ <!-- header("https://frontex-tcc.netlify.app/Frontex/public_html/view/v-home.php"); -->
+<p>Teste</p>
